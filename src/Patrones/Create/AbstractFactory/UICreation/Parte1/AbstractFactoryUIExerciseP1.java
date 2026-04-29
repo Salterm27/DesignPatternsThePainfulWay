bf4b3a78490360879ee753c13d06b23576c8b280
Salterm27 <sebastian.alterman@gmail.com>
2026-04-28 21:32:13 -0300

    Corrige la salida del texto en consola para la Parte 1 del patrón Abstract Factory y agrega ejemplo de ejecución con renderizado de interfaz.

--- src/Patrones/Create/AbstractFactory/UICreation/Parte1/AbstractFactoryUIExerciseP1.java
+++ src/Patrones/Create/AbstractFactory/UICreation/Parte1/AbstractFactoryUIExerciseP1.java
@@ -10,7 +10,10 @@ public class AbstractFactoryUIExerciseP1 implements Exercise {
 
     @Override
     public void run() {
-        System.out.println("Ejercicio Factory - Parte 1");
+        System.out.println("Ejercicio Abstract Factory - Parte 1");
+        System.out.println("----------------------------------------");
+        MyDesktopApplicationP1 app = new MyDesktopApplicationP1();
+        app.renderUI();
 
     }
 }
